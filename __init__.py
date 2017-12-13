fe10219c0caa54a1d5ed354a90a72dc8fc93a98d
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:33:06 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -1,13 +1,17 @@
 """Pytsite Hreflang Plugin
 """
-from ._api import add, get, get_all, reset
-
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
+from pytsite import plugman as _plugman
 
-def _init():
+if _plugman.is_installed(__name__):
+    # Public API
+    from ._api import add, get, get_all, reset
+
+
+def plugin_load():
     from pytsite import events, metatag, lang, router
 
     def metatag_dump_all_eh():
@@ -16,7 +20,4 @@ def _init():
                 metatag.t_set('link', rel='alternate', href=href, hreflang=lng)
 
     router.on_dispatch(reset)
-    events.listen('pytsite.metatag.dump_all', metatag_dump_all_eh)
-
-
-_init()
+    events.listen('pytsite.metatag@dump_all', metatag_dump_all_eh)
